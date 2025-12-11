3eab10f5276513aff7cb67078c93ba927570879b
Vitaliy Kishikov <naradarn@gmail.com>
2025-12-11 13:05:18 +0000

    added 500 to retry statuses

--- validator/platform_client.py
+++ validator/platform_client.py
@@ -48,7 +48,7 @@ class APIPlatformClient:
         retry = Retry(
             total=10,
             backoff_factor=0.5,
-            status_forcelist=[502, 503, 504],
+            status_forcelist=[500, 502, 503, 504],
         )
 
         session.mount("https://", HTTPAdapter(max_retries=retry))
